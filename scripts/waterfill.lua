6a56c97294a353d26ce53f2cb60e879c0cd69fb7
RedRafe <gabri97.gt+redrafe@gmail.com>
2023-10-15 12:19:07 +0200

    Update waterfill shortcut

--- scripts/waterfill.lua
+++ scripts/waterfill.lua
@@ -1,5 +1,5 @@
 local REACH = 20
-local RED = {r = 1, g = 0, b = 0}
+local TRUSTED_TIME = 15 * 60 * 60 -- 15 min
 
 local function dist(pos1, pos2)
   local x1 = pos1.x or pos1[1]
@@ -9,17 +9,30 @@ local function dist(pos1, pos2)
   return ((x1-x2)^2 + (y1-y2)^2)^0.5
 end
 
+local function flying_error(player, text)
+  player.create_local_flying_text({
+    color = {1, 0, 0, 0.5},
+    create_at_cursor = true,
+    position = player.position,
+    text = text,
+  })
+  player.play_sound({ path = "utility/cannot_build" })
+end
+
 local function edit(e)
     local player = game.get_player(e.player_index)
     local item = e.item
     if not player then return end
     if not (e.item == "kdo-waterfill") then return end
+    if player.online_time < TRUSTED_TIME then flying_error(player, {"waterfill.trusted"}) return end
 
-    local surface = e.surface
-    local tiles = e.tiles
     local area = e.area
     local pos = player.position
+    local surface = e.surface
     local entities = surface.find_entities(area)
+    if #entities > 0 then flying_error(player, {"waterfill.entities"}) return end
+
+    local tiles = e.tiles
     local new_tiles = {}
     local too_far = 0
     local t = 0
@@ -27,37 +40,15 @@ local function edit(e)
       if dist(tile.position, pos) <= REACH then
         if (tile.name and tile.name == "landfill") or (tile.hidden_tile and tile.hidden_tile == "landfill") then
           t = t + 1
-          new_tiles[t] = { name = "water", position = tile.position }
+          new_tiles[t] = { name = "water-mud", position = tile.position }
         end
       else
         too_far = too_far + 1
       end
     end
 
-    if #entities > 0 then
-      player.create_local_flying_text({
-        color = RED,
-        create_at_cursor = true,
-        position = pos,
-        text = {"text.waterfill:entities"},
-      })
-      player.play_sound({ path = "utility/cannot_build" })
-    elseif t == 0 and too_far == 0 then
-      player.create_local_flying_text({
-        color = RED,
-        create_at_cursor = true,
-        position = pos,
-        text = {"text.waterfill:not_here"}
-      })
-      player.play_sound({ path = "utility/cannot_build" })
-    elseif t == 0 and too_far > 0 then
-      player.create_local_flying_text({
-        color = RED,
-        create_at_cursor = true,
-        position = pos,
-        text = {"text.waterfill:cannot_reach"},
-      })
-      player.play_sound({ path = "utility/cannot_build" })
+    if t == 0 and too_far == 0 then flying_error(player, {"waterfill.not_here"})
+    elseif t == 0 and too_far > 0 then flying_error(player, {"waterfill.cannot_reach"})
     else
       surface.set_tiles(new_tiles)
     end
